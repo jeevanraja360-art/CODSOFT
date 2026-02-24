7dc379adec9178e2cf4fb129efdb8611263c3695
jeevanraja360-art <jeevanraja360@gmail.com>
2026-02-24 20:54:28 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,29 @@
+display.value = "Welcome 🙂";
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        let value = button.textContent;
+
+        if (value === "C") {
+            currentInput = "";
+            display.value = "";
+        } 
+        else if (value === "=") {
+            try {
+                currentInput = eval(currentInput);
+                display.value = currentInput;
+            } catch {
+                display.value = "Error";
+                currentInput = "";
+            }
+        } 
+        else {
+            currentInput += value;
+            display.value = currentInput;
+        }
+    });
+});
